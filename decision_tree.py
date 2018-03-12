9c55b67c1fab6c7326fcf1c66bad9f6337db2bd0
Adi7796 <adityamaverick8796@gmail.com>
2018-03-12 13:39:06 +0530

    classifier

---
+++ decision_tree.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+puredata=np.loadtxt('dataset2.txt',delimiter=',')
+X=puredata[:,:30]
+Y=[]
+
+for x in puredata:
+    Y.append(x[30])
+
+print "Dataset Lenght:: ", len(puredata)
+print "Dataset Shape:: ", puredata.shape
+
+X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
+
+clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100,
+                               max_depth=3, min_samples_leaf=5)
+clf_gini.fit(X_train, y_train)
+
+DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=3,
+            max_features=None, max_leaf_nodes=None, min_samples_leaf=5,
+            min_samples_split=2, min_weight_fraction_leaf=0.0,
+            presort=False, random_state=100, splitter='best')
+
+clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100,
+ max_depth=3, min_samples_leaf=5)
+clf_entropy.fit(X_train, y_train)
+
+DecisionTreeClassifier(class_weight=None, criterion='entropy', max_depth=3,
+            max_features=None, max_leaf_nodes=None, min_samples_leaf=5,
+            min_samples_split=2, min_weight_fraction_leaf=0.0,
+            presort=False, random_state=100, splitter='best')
+
+y_pred = clf_gini.predict(X_test)
+y_pred
+
+y_pred_en = clf_entropy.predict(X_test)
+y_pred_en
+
+print "Accuracy is ", accuracy_score(y_test, y_pred) * 100
+print "Accuracy is ", accuracy_score(y_test,y_pred_en)*100
